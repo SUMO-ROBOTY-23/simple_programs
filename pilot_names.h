eb6f11b128ca1478f6231b30ee2ccb24e5e6f3a6
Sara <91662997+SaLukasik@users.noreply.github.com>
2023-05-18 17:32:54 +0200

    Update pilot_names.h

--- pilot_names.h
+++ pilot_names.h
@@ -12,17 +12,17 @@
 #define p_minus	7
 #define p_plus	21
 #define p_eq	9
-#define p_0		22
+#define p_0	22
 #define p_100	25
 #define p_200	13
-#define p_1		12		
-#define p_2		24	
-#define p_3		94	
-#define p_4		8	
-#define p_5		28	
-#define p_6		90	
-#define p_7		66	
-#define p_8		82
-#define p_9		74	
+#define p_1	12		
+#define p_2	24	
+#define p_3	94	
+#define p_4	8	
+#define p_5	28	
+#define p_6	90	
+#define p_7	66	
+#define p_8	82
+#define p_9	74	
 	
 #endif
