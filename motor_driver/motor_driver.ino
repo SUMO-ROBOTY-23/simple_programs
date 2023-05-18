2af52c82f6b8f24f2c193f74dacba23cc295f9fa
Szymon Michniak <s.michniak@icloud.com>
2023-05-18 20:06:35 +0200

    Sterownie prędkością dwóch silników

--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -12,10 +12,10 @@
  * 
  * CONNECTIONS:
  * 
- * Stm32 A1  - Motor Driver PWM 1A Input
- * Stm32 A2  - Motor Driver PWM 1B Input
- * Stm32 A5  - Motor Driver PWM 2A Input
- * Stm32 A6  - Motor Driver PWM 2B Input
+ * Stm32 A1  - Motor Driver PWM D5 Input
+ * Stm32 A2  - Motor Driver PWM D6 Input
+ * Stm32 A5  - Motor Driver PWM D3 Input
+ * Stm32 A6  - Motor Driver PWM D4 Input
  * Stm32 GND - Motor Driver GND
  *
  *
@@ -28,62 +28,70 @@
 
  #include "CytronMotorDriver.h"
 
- #include <IRremote.hpp>
+#include <IRremote.hpp>
 #define IR_RECEIVE_PIN D11
 
 
 #define MINUS 0x7
 #define PLUS 0x15
+#define CHM	69
+#define CHP	71
+#define ZERO 22
 
 // Configure the motor driver.
-CytronMD motor1(PWM_PWM, A1, A2);
-CytronMD motor2(PWM_PWM, A5, A6);
+CytronMD motor1(PWM_PWM, D5, D6);
+CytronMD motor2(PWM_PWM, D3, D4);
 
-int speed = 0;
+int16_t speed1 = 0;
+int16_t speed2 = 0;
 
 // The setup routine runs once when you press reset.
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(IR_RECEIVE_PIN, INPUT);
   // initialize digital pin LED_BUILTIN as an output.
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
   pinMode(D13, OUTPUT);
   digitalWrite(D13, HIGH);   // turn the LED on (HIGH is the voltage level)
   Serial.println("Ready to read");
-  
 }
 
 
 // The loop routine runs over and over again forever.
 void loop() {
-  // 
-  
-  int data;
+    int data = 0;
     if (IrReceiver.decode()) {
       data = IrReceiver.decodedIRData.command;
 
-      Serial.println(data);
-      IrReceiver.resume();
-  }
-
+      // Serial.println(data);
+      
 
       if (data == MINUS) {
-        speed -= 10;
+        speed1 -= 1;
       } else if (data == PLUS) {
-        speed += 10;
+        speed1 += 1;
+      } else if (data == ZERO) {
+        speed1 = 0;
+        speed2 = 0;
+      } else if (data == CHP) {
+        speed2 += 1;
+      } else if (data == CHM) {
+        speed2 -= 1;
+      } else {
+        Serial.println(data);
       }
 
-      if (speed >= 256) {
-        speed = 250;
-      } else if (speed <= -256) {
-        speed = -250;
-      }
-
-      if (data == MINUS || data == PLUS) {
-        Serial.println(speed);
-        motor1.setSpeed(speed);
-        IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+      if (data == MINUS || data == PLUS || data == ZERO || data == CHM || data == CHP) {
+        Serial.println("Speeds:");
+        Serial.println(speed1);
+        Serial.println(speed2);
+        motor1.setSpeed(speed1);
+        motor2.setSpeed(speed2);
       }        
+
+      IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+    // IrReceiver.resume();
+  }
 
       
       
